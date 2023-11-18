690492042d6257412629a28f494a974c74441cae
marc <marcsmithr@gmail.com>
2023-11-18 10:10:28 -0600

    created postcard function

--- components/PostCard/index.js
+++ components/PostCard/index.js
@@ -2,3 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PostLikes from '../../Likes/PostLikes'
 import './index.css'
+
+function PostCard({post}) {
+    const user = post.user
+    const images = post.images
+
+    if(!post||!user) return null
+    return (
+
+    )
+export default PostCard
