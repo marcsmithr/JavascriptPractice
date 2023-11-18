35d3499a84efba3c4b8c0003d22dceb080bb11f0
marc <marcsmithr@gmail.com>
2023-11-18 10:14:35 -0600

    added placeholder containers

--- components/PostCard/index.js
+++ components/PostCard/index.js
@@ -30,7 +30,33 @@ function PostCard({post}) {
                                     <span>· posted by u/{user.username}</span>
                                 </div>
                             </div>
-                            
+                            <div className='post-title-container'>
+                                <span> {post.title} </span>
+                            </div>
+                            {(images[0]) &&
+                            <div className='post-image-container'>
+                                <img src={images[0].url}></img>
+                            </div>
+                            }
+                            {(post.text)&&
+                            <div className='post-card-text-container'>
+                                <span>{post.text}</span>
+                            </div>
+                            }
+                            <div className='post-interaction-container'>
+                                {/* <div className='post-card-comment-container'>
+                                    <div className='post-card-comment-icon'>
+                                        <i className="fa-regular fa-comment"></i>
+                                    </div>
+                                    <div className='post-card-comment-text'>
+                                        <span>Comments</span>
+                                    </div>
+                                </div> */}
+                                <div className='post-card-save-container'>
+
+                                </div>
+                            </div>
+
                         </div>
                     </Link>
                 </div>
